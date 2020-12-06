f03c13f0818580cfd898ff6145f922797da8418e
Vladimir Vukovic <vladimirvukovic98@hotmail.rs>
2020-12-06 20:05:59 +0100

    ObavestenjeMarshaller.java

--- xml/src/main/java/com/ftn/xml/jaxb/obavestenje/ObavestenjeMarshaller.java
+++ xml/src/main/java/com/ftn/xml/jaxb/obavestenje/ObavestenjeMarshaller.java
@@ -2,9 +2,11 @@ package com.ftn.xml.jaxb.obavestenje;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.List;
 
 import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
@@ -13,7 +15,6 @@ import javax.xml.validation.SchemaFactory;
 
 import com.ftn.jaxb.util.MyValidationEventHandler;
 import com.ftn.jaxb.util.NSPrefixMapper;
-import com.ftn.xml.jaxb.resenje.ResenjeObrazac;
 
 public class ObavestenjeMarshaller {
 
@@ -27,12 +28,12 @@ public class ObavestenjeMarshaller {
 			SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 			Schema schema = schemaFactory.newSchema(new File("./data/obavestenje.xsd"));
 
-			TObavestenje obavestenje = (TObavestenje) unmarshaller.unmarshal(new File("./data/obavestenje.xml"));
+			Obavestenje obavestenje = (Obavestenje) unmarshaller.unmarshal(new File("./data/obavestenje.xml"));
 
 			unmarshaller.setSchema(schema);
 			unmarshaller.setEventHandler(new MyValidationEventHandler());
 
-			printResenje(obavestenje);
+			printObavestenje(obavestenje);
 			
 			Marshaller marshaller = context.createMarshaller();
 			
@@ -41,7 +42,7 @@ public class ObavestenjeMarshaller {
 			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 			
 			// Umesto System.out-a, moÅ¾e se koristiti FileOutputStream
-			FileOutputStream os = new FileOutputStream(new File("./data/resenje_marshall.xml"));
+			FileOutputStream os = new FileOutputStream(new File("./data/obavestenje_marshall.xml"));
 			marshaller.marshal(obavestenje, os);
 
 		} catch (JAXBException e) {
@@ -50,8 +51,94 @@ public class ObavestenjeMarshaller {
 
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private void printObavestenje(Obavestenje o) {
+		System.out.println("Obavestenje:");
+		printOsnovniPodaci(o.getOsnovniPodaci());
+		printSadrzaj(o.getSadrzaj());
+		printPodnozje(o.getPodnozje());
+	}
+	
+	
+
+	private void printSadrzaj(Sadrzaj s) {
+		System.out.println("\tSadrzaj:");
+		System.out.println("\t\tNaslov: " + s.getNaslov());
+		printTeloObavestenja(s.getTeloObavestenja());
+	}
+
+	private void printTeloObavestenja(TeloObavestenja t) {
+		System.out.println("\t\tTelo obavestenja: ");
+		for(Paragraf p : t.getParagraf()) {
+			System.out.println("\t\t\tParagraf[" + p.getId() + "]");
+			printParagraf(p.getContent());
+		}
+	}
+	
+	private void printSluzbeniGlasnik(SluzbeniGlasnik g) {
+		System.out.println("\t\t\t\tSluzbeni glasnik broj: " + g.getBroj());
+	}
+
+	private void printParagraf(List<Object> content) {
+		for(Object o : content) {
+			if(o instanceof Zakon) {
+				
+			} else if(o instanceof Adresa) {
+				Adresa a = (Adresa)o;
+				System.out.println("\t\t\t\tAdresa: ");
+				System.out.println("\t\t\t\t\tGrad: " + a.getGrad());
+				System.out.println("\t\t\t\t\tUlica: " + a.getUlica());
+				System.out.println("\t\t\t\t\tBroj: " + a.getBroj());
+			} else if(o instanceof SluzbeniGlasnik) {
+				SluzbeniGlasnik s = (SluzbeniGlasnik) o;
+				printSluzbeniGlasnik(s);
+			} else if(o instanceof ZiroRacun) {
+				ZiroRacun z = (ZiroRacun) o;
+				System.out.println("\t\t\t\tZiro racun:");
+				System.out.println("\t\t\t\t\tVrednost: " + z.getValue());
+				System.out.println("\t\t\t\t\tPoziv na broj: " + z.getPozivNaBroj());
+			} else if(o instanceof String) {
+				System.out.println("\t\t\t\t" + o);
+			} else {
+				JAXBElement el = (JAXBElement) o;
+				System.out.println("\t\t\t\t" + el.getName());
+				System.out.println("\t\t\t\t\t" + el.getValue());
+			}
+		}
+	}
+
+	private void printPodnozje(Podnozje p) {
+		System.out.println("\tPodnozje:");
+		System.out.println("\t\tDostavljeno:");
+		for(Lice l : p.getDostavljeno().getLice()) {
+			System.out.println("\t\t\tLice[" + l.getId() + "]: " + l.getValue());
+		}
+		System.out.println("\t\tPotpis i pecat" + p.getPotpisOvlascenogLica());
+	}
+
+	private void printOsnovniPodaci(OsnovniPodaci osnovniPodaci) {
+		System.out.println("\tOsnovni podaci:");
+		printPodaciOOrganu(osnovniPodaci.getPodaciOOrganu());
+		printPodaciOPodnosiocu(osnovniPodaci.getPodaciOPodnosiocu());
+	}
+
+	private void printPodaciOPodnosiocu(PodaciOPodnosiocu p) {
+		System.out.println("\t\tPodaci o podnosiocu:");
+		System.out.println("\t\t\tIme i prezime: " + p.getImeIPrezime());
+		System.out.println("\t\t\tNaziv: " + p.getNaziv());
+		System.out.println("\t\t\tAdresa: " + p.getAdresa());
+	}
+
+	private void printPodaciOOrganu(PodaciOOrganu p) {
+		System.out.println("\t\tPodaci o organu:");
+		System.out.println("\t\t\tNaziv: " + p.getNaziv());
+		System.out.println("\t\t\tSediste: " + p.getSediste());
+		System.out.println("\t\t\tBroj predmeta: " + p.getBrojPredmeta());
+		System.out.println("\t\t\tDatum: " + p.getDatum());
+	}
+
+	public static void main(String[] args) throws Exception {
+		ObavestenjeMarshaller o = new ObavestenjeMarshaller();
+		o.test();
 
 	}
 
